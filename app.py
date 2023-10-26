2afba04bc14e7bb091caf9a095f3e7a9fdf54e0e
Fredy <fjarava@utb.edu.co>
2023-10-26 16:51:09 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, Form, Request, status
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    print('Request for index page received')
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    print('Request for index page received')
+    return templates.TemplateResponse('index.html', {"request": request})
+
+@app.post('/hello', response_class=HTMLResponse)
+def hello(request: Request, name: str = Form(...)):
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return templates.TemplateResponse('hello.html', {"request": request, 'name': name})
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+
+
+@app.get("/")
+def read_root():
+    return {"200": "Welcome To Student Restful API"}
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run('app:app')
